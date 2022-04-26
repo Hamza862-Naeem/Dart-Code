87d86ed28fe47151aa198bac9cbac6351e8b93fc
Hamza862-Naeem <h.mhamzanaeem862@gmail.com>
2022-04-26 11:39:00 +0500

    Second Commit

--- loops.dart
+++ loops.dart
@@ -1,13 +1,29 @@
 
-//While loop
+//While loop pre test loop (runs 0 or more time)
 void main(){
 
-int x =0;  //declare your loop control
+int x =0;  //declare your loop control 
 while(x<5)   {      //Test loop control variable
   print("Hello World");
   x++; //Change the loop control variable
 //x+=2;
 }
-//
+
+//do while
+// loop post test loop (runs 1 or more times)
+
+int y=0;
+do {
+  print("Hello Kito");
+  y++;
+}
+while (y<3);
+
+//for loop
+// pre test loop (runs 0 or more times)
+   
+   for(int i=0; i<4; i++ ){
+     print("Hey Machu Pichu");
+   }
 
 }
