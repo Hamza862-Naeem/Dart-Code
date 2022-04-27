cca61b337b3f03f489aa56819a597348c1b999f0
Hamza862-Naeem <h.mhamzanaeem862@gmail.com>
2022-04-27 10:36:15 +0500

    Sets

---
+++ Sets.dart
@@ -0,0 +1,39 @@
+//sets are collection of unique 
+//means same value can not repeat
+//list can contain duplicate values
+
+void  main() {
+  var Lang={"Russian","Urdu","French","Urdu"};
+  print(Lang);
+
+//methods in Sets list like 
+
+ //lang.add("Spanish");
+//lang.remove("Russian");
+//print(lang);
+ // print(lang.first);
+ // print(lang.length);
+ // print(lang.contains("Urdu"));
+ // print(lang.isEmpty);
+
+
+
+   //Unique sets methods
+
+//print(lang.elementAt(0));
+
+ // var chLanguages={"Mandarian","Yue","French","Wu","Minbei"};
+//print(chLanguages.union(Lang));
+//print(chLanguages.intersection(Lang));
+//print(chLanguages.difference(lang));
+
+
+
+ // for (var lang in lang){
+   // print(lang); }
+  
+  
+  
+  }
+
+
