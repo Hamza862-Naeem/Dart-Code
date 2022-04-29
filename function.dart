5ee99f8ad8b0397cc3a343509a033c6fd1bf9a3e
Hamza862-Naeem <h.mhamzanaeem862@gmail.com>
2022-04-29 10:15:56 +0500

    maps commit

---
+++ function.dart
@@ -0,0 +1,17 @@
+//Functions are "self contained" modules of code that
+// accomplish a specific task. 
+//Function usually takes in data , process it , and
+//return a result . Once it written it can be used over 
+// and over again. Function can be called from inside
+// of other function. Use function to organize your code
+// break code into smaller pieces and it reusable
+
+//The main() function is predefined method in Dart.
+//The method act as entry point to application.
+
+
+// Note: Methods belong to object of Class
+
+void main (){
+
+}
---
+++ maps.dart
@@ -0,0 +1,36 @@
+
+//Collection of key value pairs
+
+
+void main (){
+var user ={
+  'username' : 'peter',
+  'password' : 'peter1234',
+  'role'   :  'admin',
+  'staffNr' :   9912,
+};
+print(user);
+  //print(user.values);
+// Getting values using keys (individual values )
+var school = user['role'];
+ print(school);
+
+
+
+
+
+
+// Explicitly declaring a map
+  Map <String, String> user2= {
+          'username'  : 'james',
+          'password' : 'jimmy54321',
+          'role'    :   'clerk' ,
+          'staffNr'  :  '8787'
+  };
+    print(user2);
+  //set a new value for key
+  
+
+
+}
+
